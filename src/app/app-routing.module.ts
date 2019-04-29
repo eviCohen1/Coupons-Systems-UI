87b24b6ef3b3cc3773c3a6e586957f9c6e3c3933
eviCohen1 <evi.cohen1@gmail.com>
2019-04-29 15:27:03 +0300

    Update Haeader component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AdminComponent } from './components/admin/admin.component';
 import { CompanyComponent } from './components/company/company.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { CouponDetailComponent } from './components/company/Coupons/coupon-detail.component';
+import { couponListComponent } from './components/company/Coupons/coupon-list.component';
 
 const routes: Routes = [
   {path : "home", component: HomeComponent},
   {path : "admin", component: AdminComponent},
   {path : "company", component: CompanyComponent},
   {path : "customer", component: CustomerComponent},
+  { path: 'coupons', component: couponListComponent},
   { path: 'coupon/:id',component:CouponDetailComponent},
 ];
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { timeInterval } from 'rxjs/operators';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public currentDate: Date;
+  public temp; 
+  public currentTime: Date;
   constructor() { }
 
   ngOnInit() {
+
+    this.currentDate =new Date(); 
+    this.currentTime = new Date(); 
+    this.temp = 25.5;
   }
 
 }
